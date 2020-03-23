5c2e28ad03d2981d520c3a89c6a114b079bbb321
OlegSvrk <serv_all@mail.ru>
2020-03-23 23:54:49 +0300

    Очередная проба списков и наследования. Многопоточка пока не пошла(

--- tryLists/Program.cs
+++ tryLists/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace tryLists
 {
-
     class Department
     {
         public string DepartmentName { get; set; }
@@ -18,51 +17,99 @@ namespace tryLists
 
     }
 
+    struct Address
+    {
+        public uint Code;
+        public string Country;
+        public string City;
+        public string Street;
+    }
 
-    class Employee
+    class Human
+    {
+        Random rnd = new Random();
+        public int Age { get; set; }
+        public int Weigth { get; set; }
+        public Address address = new Address();
+
+        public virtual void TestingWrite()
+        {
+            Console.WriteLine("Testing Write from HumanClass");
+        }
+    }
+
+
+    class Employee: Human
     {
         public string Name { get; set; }
-        public int? Age { get; set; }
+        new public int? Age { get; set; }
 
         public Employee()
         {
-            this.Name = "Unknown";
-            this.Age = null;
+            Name = "Unknown";
+            Age = null;
         }
         public Employee(string Name)
         {
             this.Name = Name;
-            this.Age = null;
+            Age = null;
         }
         public Employee(string Name, int Age)
         {
             this.Name = Name;
             this.Age = Age;
         }
+
+        public override void TestingWrite()
+        {
+            Console.WriteLine("Testing Write from EmployeeClass");
+            base.TestingWrite();
+        }
+
+        
+
     }
 
+    public class TClass
+    {
 
+    }
 
 
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
+
+            
+
+
+
             List<Employee> employees = new List<Employee>
             {
                 new Employee("Alice", 19),
                 new Employee("Bob", 21),
-                new Employee("Chack", 55)
+                new Employee("Ched", 55)
             };
 
-
             Console.WriteLine($"Записей всего: {employees.Count}");
+            employees.Add(new Employee("Diana", 48));
 
+            Console.Write("Enter zip-code for employee #2: ");
+            employees[1].address.Code = (uint)Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"{employees[1].Name} is {employees[1].Age }");
+
+            Console.Write("Enter new name for employee #1: ");
             employees[0].Name = Console.ReadLine();
 
-            foreach (Employee human in employees) Console.WriteLine(human.Name + " is " + human.Age);
+            employees[0].TestingWrite();
+
+            foreach (Employee human in employees)
+                Console.WriteLine(human.Name + " is " + human.Age);
 
             Console.ReadKey();
         }
